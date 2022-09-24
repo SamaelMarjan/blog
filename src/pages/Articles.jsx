6a38ee438588f950fb78500a8936dadbbc3632b7
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-25 02:40:15 +0600

    article-sept-24-20-34

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,8 +1,320 @@
 import React from 'react';
 
+import Img from '../assets/Article_Image.png';
+import Img1 from '../assets/Article_Image1.png';
+import Img2 from '../assets/Article_Image2.png';
+import Img3 from '../assets/Article_Image3.png';
+
 const Articles = () => {
   return (
-    <div>Articles</div>
+    <div className='w-[100%] h-[100%]'>
+      <div className='w-[90%] h-[100%] m-auto'>
+        <div className='w-[100%] h-[100%] flex flex-col gap-4'>
+          <div className='w-[100%] h-[50px] flex items-center justify-between'>
+            <p>Featured Article</p>
+            <p>See All Article</p>
+          </div>
+          <div className='grid grid-cols-3 md:flex gap-5 md:gap-10 items-center justify-center'>
+            <div>
+              <div>
+                <div>
+                  <img src={Img} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img1} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img2} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img3} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-[100%] h-[100%] flex flex-col gap-4'>
+          <div className='w-[100%] h-[50px] flex items-center justify-between'>
+            <p>Featured Article</p>
+            <p>See All Article</p>
+          </div>
+          <div className='grid grid-cols-3 md:flex gap-5 md:gap-10 items-center justify-center'>
+            <div>
+              <div>
+                <div>
+                  <img src={Img} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img1} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img2} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img3} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-[100%] h-[100%] flex flex-col gap-4'>
+          <div className='w-[100%] h-[50px] flex items-center justify-between'>
+            <p>Featured Article</p>
+            <p>See All Article</p>
+          </div>
+          <div className='grid grid-cols-3 md:flex gap-5 md:gap-10 items-center justify-center'>
+            <div>
+              <div>
+                <div>
+                  <img src={Img} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img1} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img2} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img3} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-[100%] h-[100%] flex flex-col gap-4'>
+          <div className='w-[100%] h-[50px] flex items-center justify-between'>
+            <p>Featured Article</p>
+            <p>See All Article</p>
+          </div>
+          <div className='grid grid-cols-3 md:flex gap-5 md:gap-10 items-center justify-center'>
+            <div>
+              <div>
+                <div>
+                  <img src={Img} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img1} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img2} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img3} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-[100%] h-[100%] flex flex-col gap-4'>
+          <div className='w-[100%] h-[50px] flex items-center justify-between'>
+            <p>Featured Article</p>
+            <p>See All Article</p>
+          </div>
+          <div className='grid grid-cols-3 md:flex gap-5 md:gap-10 items-center justify-center'>
+            <div>
+              <div>
+                <div>
+                  <img src={Img} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img1} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img2} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <img src={Img3} alt='article' />
+                </div>
+                <div>
+                  <p>Fundamental of javascript</p>
+                </div>
+              </div>
+              <div>
+                <p>Samael</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 const Category = () => {
   return (
-    <div id='cat'>Category</div>
+    <div id='cat' className='w-[100%] h-[100%]'>
+      <div className='w-[90%] h-[100%] m-auto flex flex-col gap-4 '>
+        <div className='flex items-center justify-between mt-8'>
+          <p>Browse The Categoory</p>
+          <p>See All Category</p>
+        </div>
+        <div className='w-[100%] h-[100%] flex flex-wrap md:flex-row items-center gap-6 mb-8'>
+          <div className='w-[100px] md:w-[220px] h-[150px] md:h-[280px] bg-[#fff] border-2 rounded-lg hover:bg-[#6246EA]'>
+            <p>CSS</p>
+          </div>
+          <div className='w-[100px] md:w-[220px] h-[150px] md:h-[280px] bg-[#fff] border-2 rounded-lg hover:bg-[#6246EA]'>
+            <p>CSS</p>
+          </div>
+          <div className='w-[100px] md:w-[220px] h-[150px] md:h-[280px] bg-[#fff] border-2 rounded-lg hover:bg-[#6246EA]'>
+            <p>CSS</p>
+          </div>
+          <div className='w-[100px] md:w-[220px] h-[150px] md:h-[280px] bg-[#fff] border-2 rounded-lg hover:bg-[#6246EA]'>
+            <p>CSS</p>
+          </div>
+          <div className='w-[100px] md:w-[220px] h-[150px] md:h-[280px] bg-[#fff] border-2 rounded-lg hover:bg-[#6246EA]'>
+            <p>CSS</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
